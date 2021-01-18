46470dc310bc7ad1271af329974982566db1ca49
Inchijeong <cde0409@naver.com>
2021-01-18 10:38:53 +0900

    정렬 방법 기초

---
+++ src/base/OverrideComparator.java
@@ -0,0 +1,37 @@
+package base;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class OverrideComparator {
+	public static void main(String[] args) {
+		
+//		Integer[] arr = new Integer[5];
+//		
+//		for(int i = 0; i < arr.length; i++) {
+//			arr[i] = i;
+//		}
+		
+		Integer[] arr = {2, 1, 3, 5, 6};
+		
+        Arrays.sort(arr, new Comparator<Integer>() {
+        	@Override
+        	public int compare(Integer i1, Integer i2) {
+        		
+        		int res = 0;
+        		
+        		// 오름차순
+        		if(i1 > i2) {
+        			res = 1;
+        		}else if(i1 < i2) {
+        			res = -1;
+        		}
+        		
+        		return res;
+        	}
+        });
+        
+        System.out.println(Arrays.toString(arr));
+        
+	}
+}
