8de3fd6829d98042ea84f6cd0d76e4dd6d9eafeb
Inchijeong <cde0409@naver.com>
2021-01-21 01:10:13 +0900

    여행 경로 내 풀이로 변경

--- src/programmers/kit/bfs_dfs/travelRoute/Solution.java
+++ src/programmers/kit/bfs_dfs/travelRoute/Solution.java
@@ -6,39 +6,52 @@ import java.util.Collections;
 
 public class Solution {
 	static boolean[] visited;
-	static ArrayList<String> answers;
+	static ArrayList<String> answers = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		
-		String[][] tickets = {{"ICN", "JFK"}, {"HND, IAD"}, {"JFK, HND"}};
+//		String[][] tickets = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
+		String[][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL", "SFO"}};
 		
 		System.out.println("answer: " + Arrays.toString(solution(tickets)));
 	}
 	
-	public static String[] solution(String[][] tickets) {
+	public static String[] solution(String[][] tickets) {		
+		// 방문 체크
 		visited = new boolean[tickets.length];
-		answers = new ArrayList<String>();
+		// 시작 공항
+		String origin = "ICN";
+		// 전체 티켓 수
+		int n = tickets.length;
+		// 사용한 티켓의 수
 		int count = 0;
-		dfs(count, "ICN", "ICN", tickets);
+		
+		dfs(tickets, origin, count, n, origin);
+		// 알파벳 순서로 뽑기 위해 정렬
 		Collections.sort(answers);
+		// 가장 먼저인 경로를 배열로 변환
 		String[] answer = answers.get(0).split(" ");
 		
 		return answer;
 	}
 	
-	public static void dfs(int count, String present, String answer, String[][] tickets) {
-		if(count == tickets.length) {
-			answers.add(answer);
+	public static void dfs(String[][] tickets, String origin, int count, int n, String ans) {
+
+		System.out.println(origin);
+		if(count == n) {
+			System.out.println("모든 티켓 사용");
+			System.out.println(ans);
+			answers.add(ans);
 			return;
 		}
-		for(int i = 0; i < tickets.length; i++) {
-			if(!visited[i] && tickets[i][0].equals(present)) {
-				System.out.println(answer);
+		
+		for(int i = 0; i < n; i++) {
+			if(!visited[i] && tickets[i][0].equals(origin)) {
+				String dest = tickets[i][1];
 				visited[i] = true;
-				dfs(count+1, tickets[i][1], answer+" "+tickets[i][1], tickets);
-				visited[i] = false;
+				dfs(tickets, dest, count+1, n, ans + " " + dest);
+				visited[i] = false; // 다른 티켓을 찾을때 방문을 초기화하기 위함
 			}
 		}
-		return;
 	}
 }
