f617c0050c5e823378801444a78eecdc5476cedc
Inchijeong <cde0409@naver.com>
2021-01-29 15:07:53 +0900

    순위 풀이 추가

---
+++ src/programmers/kit/graph/q2_ranking/Ranking.java
@@ -0,0 +1,17 @@
+package programmers.kit.graph.q2_ranking;
+
+public class Ranking {
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		int[][] results = {{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}};
+		
+		System.out.println("answer: " + solution(n, results));
+	}
+	
+	public static int solution(int n, int[][] results) {
+		int answer = 0;
+		return answer;
+	}
+}
