1e4a2b16f910d364e8df17ce90bdf3dbee279119
Inchijeong <cde0409@naver.com>
2021-02-23 06:45:22 +0900

    방의 개수 풀이 추가

---
+++ src/programmers/kit/graph/q3_number_of_rooms/NumberOfRooms.java
@@ -0,0 +1,21 @@
+package programmers.kit.graph.q3_number_of_rooms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberOfRooms {
+
+	public static void main(String[] args) {
+		
+		int[] arrows = {6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0};
+		
+		System.out.println("answer: " + solution(arrows));
+	}
+	
+	public static int solution(int[] arrows) {
+		int answer = 0;
+		List check = new ArrayList();
+
+		return answer;
+	}
+}
