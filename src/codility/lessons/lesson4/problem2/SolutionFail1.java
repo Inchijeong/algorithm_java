b1fd884951002c34f750fa67ee19248ff61b5d9c
Chijeong In <cde0409@naver.com>
2025-05-09 23:56:11 +0900

    코드리티 레슨 8 문제 1에 대한 솔루션 클래스 추가(실패)

---
+++ src/codility/lessons/lesson4/problem2/SolutionFail1.java
@@ -0,0 +1,22 @@
+package codility.lessons.lesson4.problem2;
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        // Implement your solution here
+        int result = 1;
+        Arrays.sort(A);
+        int pre = A[0];
+        for (int i = 1; i < A.length; i++) {
+            if (pre + 1 != A[i]) {
+                return 0;
+            }
+            pre++;
+        }
+        return result;
+    }
+}
