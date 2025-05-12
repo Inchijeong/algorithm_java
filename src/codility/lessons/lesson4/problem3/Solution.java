7dd987e478415d6431b0190e2edd5efd39dde5d4
Chijeong In <cde0409@naver.com>
2025-05-12 23:57:43 +0900

    코드리티 레슨 4 문제 3에 대한 솔루션 클래스 추가

---
+++ src/codility/lessons/lesson4/problem3/Solution.java
@@ -0,0 +1,29 @@
+package codility.lessons.lesson4.problem3;
+
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int N, int[] A) {
+        // Implement your solution here
+        int[] result = new int[N];
+        int max = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            int X = A[i];
+            if (X >= 1 && X < N) {
+                result[X - 1] += 1;
+                if (max < result[X - 1]) {
+                    max = result[X - 1];
+                }
+                System.out.print(max);
+            }
+            if (X == N + 1) {
+                for (int j = 0; j < result.length; j++) {
+                    result[j] = max;
+                }
+            }
+            System.out.println(Arrays.toString(result));
+        }
+        return result;
+    }
+}
